888d3ca41212693f53e8c5b6a01befdd6bb10787
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-01-19 18:30:03 +0100

    Added script to build solution files

---
+++ Setup-Windows.bat
@@ -0,0 +1,27 @@
+@echo off
+
+echo Choose an IDE to build:
+echo vs2022
+echo vs2019
+echo vs2017
+echo vs2015
+echo vs2013
+echo vs2012
+echo vs2010
+echo vs2008
+echo vs2005
+echo gmake
+echo gmake2
+echo xcode4
+echo codelite
+
+set /p choice="Enter your choice (e.g., 1 for vs2022): "
+
+
+:: Get the selected IDE from the array
+call premake5 %choice%
+
+echo Premake setup completed!
+
+:: Pause the script to see the output before closing
+pause
