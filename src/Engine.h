e05dbf907861da30a9b0074cc23c1ee46f2aa758
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-22 09:44:27 +0200

    Made camera settings non static

--- src/Engine.h
+++ src/Engine.h
@@ -4,10 +4,21 @@
 // settings
 unsigned int SCR_WIDTH = 1160;
 unsigned int SCR_HEIGHT = 860;
-static float nearClip = 0.1f;
-static float farClip = 100.0f;
-static float fieldOfView = 50.0f;
+float nearClip = 0.1f;
+float farClip = 100.0f;
+float fieldOfView = 50.0f;
 float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+
+struct WindowSettings
+{
+	unsigned int screenWidth = 1160;
+	unsigned int screenHeight = 860;
+};
+
+struct CameraSettings
+{
+
+};
 
 // Background color values 
 ImVec4 clear_color = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
