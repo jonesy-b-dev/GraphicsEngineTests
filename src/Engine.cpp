89d98f433206e70c929521d9f515790522bfb43b
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-06-12 14:51:32 +0200

    Moved some settings to the engine.h file

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -18,25 +18,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
-
-// camera
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
-
-glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
-
-bool firstMouse = true;
-float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
-float pitch =  0.0f;
-float lastX =  800.0f / 2.0;
-float lastY =  600.0 / 2.0;
-float fov   =  45.0f;
-
-// timing
-float deltaTime = 0.0f;	// time between current frame and last frame
-float lastFrame = 0.0f;
-
 int main()
 {   
 	#pragma region Initialisation
@@ -48,6 +29,12 @@ int main()
 	
     glfwSetCursorPosCallback(WindowManager::GetWindow(), mouse_callback);
     glfwSetScrollCallback(WindowManager::GetWindow(), scroll_callback);
+
+
+	/// Test config
+	std::cout << config.GetInt("window", "height") << "\n";
+	config.WriteInt("window", "height", 200);
+	std::cout << config.GetInt("window", "height") << "\n";
 
 
     // tell GLFW to capture our mouse
--- src/Engine.h
+++ src/Engine.h
@@ -9,6 +9,25 @@ float farClip = 100.0f;
 float fieldOfView = 50.0f;
 float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
 
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+
+glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+
 struct WindowSettings
 {
 	unsigned int screenWidth = 1160;
